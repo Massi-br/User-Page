c9a62df4302b9c5fa10beec5fe9b8e824dde1788
MassiBr <massikaneki09@gmail.com>
2024-03-09 02:37:34 +0100

    creation de la page des annonces

--- src/Components/annonces.jsx
+++ src/Components/annonces.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "./style.css"
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 function ListeAnnonces() {
+
+    const annonce={
+        title :"Annonce 1",
+        desc:"azertyui123",
+        price:"123$"
+    }
+
     return<>
-    <h1>Houraaaaa
-        !!!!!</h1>
+    <div className="annoncesContainer">
+        <h5>Titre : </h5><p>{annonce.title}</p>
+        <h5>description : </h5><p>{annonce.desc}</p>
+        <h5>prix : </h5><p>{annonce.price}</p>
+
+        <Link to="/">
+              <Button variant="contained" className="annonceBtn">
+                Modifier
+              </Button>
+        </Link>
+        <Link to="/">
+              <Button variant="contained" className="annonceBtn">
+                Supprimer
+              </Button>
+        </Link>
+    </div>
+
     
     </>
 }
